2fc9732412c05ce7b7d18339f0d509bcb16088bd
Rafael Massaro <rafaelmassaro9@gmail.com>
2021-06-22 11:16:22 -0300

    Implemented Create User

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { CreateUserServices } from "../services/CreateUserService";
+
+
+class CreateUserController{
+
+    async handle(request: Request, response: Response){
+        const { name, email, admin } = request.body;
+
+        const createUserService = new CreateUserServices();
+
+        const user = await createUserService.execute({ name, email, admin });
+
+        return response.json(user);
+    }
+}
+
+export { CreateUserController }
